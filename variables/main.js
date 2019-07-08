641a96c1b9d7f51f591da7cf52a9d9cfc486078a
Iurii Diatlov <tgmotw@yahoo.com>
2019-07-09 04:57:48 +1000

    Initial commit Variables

---
+++ variables/main.js
@@ -0,0 +1,29 @@
+//Задание №1
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageScore;
+
+averageScore = (algebra + geography + physics) / 3;
+
+console.log(`Средняя оценка по трём предметам ${averageScore}`);
+
+//Задание №2
+let name = 'Yury';
+let message;
+
+message = `Привет мир! Меня зовут ${name}`;
+
+console.log(message);
+
+//Задание №3
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+
+result =  x * y + 5 * z + x - 1;
+
+console.log(`x * y + 5 * z + x - 1 = ${result}, где х=${x}, y=${y}, z =${z}`);
+
+
