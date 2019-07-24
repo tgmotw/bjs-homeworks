44e3bda0f2877f07d1e8d91a71ec63b81bb3f418
Iurii Diatlov <tgmotw@yahoo.com>
2019-07-25 05:55:52 +1000

    Object-Prototype initial commit

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,15 +1,4 @@
-function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
-
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
-
-    document.getElementById('disclaimer').innerHTML = result;   
-}
-
-function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
-}
-
+//Задача №1
 function initPrintAnimalSound() {
     const animal = {
         sound: 'grrrr',
@@ -21,19 +10,49 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+    if (animal === undefined){return null};
+    let sound = animal.sound;
+    return  sound;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Задача №2
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
         const marks = document.getElementById('learner-' + idx).value.split(',');
-
         const average = getAverageMark(marks);
 
         document.getElementById('learner-' + idx + '-average').innerHTML = average;
-    }
-}
+    };
+};
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+    let average = 0;
+    for (mark of marks){
+        average = average + mark * 1; // mark * 1 - Приводим к number
+    };
+	
+    return average / marks.length;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Задача №3
+function initCheckBirthday() {
+    const birthday = document.getElementById('birthday').value;
+
+    const result = checkBirthday(birthday) ? "Да" : "Нет";
+
+    document.getElementById('disclaimer').innerHTML = result;
+};
+
+function checkBirthday(birthday) {
+    let now = new Date();
+    let usesrbirthday = new Date(birthday);
+	
+	if (now.getFullYear() - usesrbirthday.getFullYear() > 18) {return true};
+	if (now.getFullYear() - usesrbirthday.getFullYear() < 18) {return false};
+	if (now.getFullYear() - usesrbirthday.getFullYear() === 18 && now.getMonth() - usesrbirthday.getMonth() >= 1) {return true};
+	if (now.getFullYear() - usesrbirthday.getFullYear() === 18 && now.getMonth() - usesrbirthday.getMonth() === 0 && now.getDate() - usesrbirthday.getDate() >= 0) {return true};
+	
+	return false;
 }
